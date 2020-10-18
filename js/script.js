91ff43093f7ac1aebe4e63fafdef4a1da8f8abc9
Oleh Skrypnykov <oskripnikov@gmail.com>
2020-10-18 12:02:14 +0300

    update media request

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
-    $('.gallery-slider-list').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        responsive: [
-            {
-              breakpoint: 650,
-                settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-                arrows: false,
-              }
-            },
-            {
-              breakpoint: 450,
+  $('.gallery-slider-list').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      responsive: [
+          {
+            breakpoint: 650,
               settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                arrows: false,
-              }
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              arrows: false,
             }
-        ]
-    });
+          },
+          {
+            breakpoint: 450,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows: false,
+            }
+          }
+      ]
   });
+});
